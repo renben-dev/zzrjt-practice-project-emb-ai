65e44905bb5a5603d6ef5d7b5a57e65ca65cb255
renben-dev <bidone.renzo@gmail.com>
2025-05-14 08:21:40 -0400

    rolling update

---
+++ sentiment_analysis.py
@@ -0,0 +1,14 @@
+
+import requests, json
+
+def sentiment_analyzer(text_to_analyze):
+    print(text_to_analyze)
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    headers= {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    in_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = in_json, headers = headers)
+    dict_response =json.loads(response.text)
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+    return {label, score}
